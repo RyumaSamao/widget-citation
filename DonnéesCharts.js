cb191c3c50b4c6f6c699b35a2aa47e4a55f3fb33
RyumaSamao <mathysalloy@gmail.com>
2026-06-09 22:14:33 +0200

    Create DonnéesCharts.js

---
+++ DonnéesCharts.js
@@ -0,0 +1,37 @@
+const WATCHLIST_DATA = {
+  anime: {
+    InProgress: 64,
+    Paused: 18,
+    NotStarted: 97,
+    Rewatching: 12,
+    Dropped: 9,
+    Completed: 102
+  },
+
+  animation: {
+    InProgress: 20,
+    Paused: 5,
+    NotStarted: 30,
+    Rewatching: 2,
+    Dropped: 1,
+    Completed: 40
+  },
+
+  series: {
+    InProgress: 15,
+    Paused: 3,
+    NotStarted: 22,
+    Rewatching: 4,
+    Dropped: 2,
+    Completed: 60
+  },
+
+  movies: {
+    InProgress: 6,
+    Paused: 1,
+    NotStarted: 10,
+    Rewatching: 0,
+    Dropped: 1,
+    Completed: 25
+  }
+};
